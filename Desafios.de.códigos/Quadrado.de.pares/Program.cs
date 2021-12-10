b1b3ea0e5b717b8ee8c1c48afab5360139194247
Jonas Felix <jonas.medeiros.felix@gmail.com>
2021-12-10 15:00:49 -0300

    2 - Desafio Quadrado de Pares

---
+++ Desafios.de.códigos/Quadrado.de.pares/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+using System; 
+
+class DIO {
+
+    static void Main(string[] args) { 
+
+      int n = int.Parse(Console.ReadLine());
+            for(          )
+            {
+                int b =    ;          //escreva o seu código nos espaços em branco
+                b =          ;
+                Console.WriteLine(                 );
+            }
+
+    }
+
+}
+*/
+using System;//FEITO
+class DIO
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()); 
+        if( n > 5 && n < 2000)
+        {
+            int quadradoPares = 0;       
+            for(int i = 1; (2*i) <= n; i++ )
+            {
+                quadradoPares = (2*i)*(2*i);
+                Console.WriteLine($"{2*i}^2 = {quadradoPares}");
+            }
+        }
+    }
+}
