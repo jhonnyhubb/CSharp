a0be29764b5d17ce7fef572c5cc7ea9a43cfe50f
Jonas Felix <jonas.medeiros.felix@gmail.com>
2021-12-10 14:39:16 -0300

    a study of builders and properties

---
+++ Construtores/ExemploConstrutores/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+        {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            Console.WriteLine(pi);
+            
+            // Data data = new Data();
+            // //data.SetMes(20);
+
+            // data.Mes = 12;
+            // System.Console.WriteLine(data.Mes);
+
+            // data.ApresentarMes();
+            // Log log = log.GetInstance();
+            // log.PropriedadeLog = "teste de instancia";
+
+            // Log log2 = log.GetInstance();
+            // System.Console.WriteLine(log2.PropriedadeLog);
+
+            // Aluno p1 = new Aluno( "Bob" , "Marley", "disciplina");
+            // p1.Apresentar();
+            
+        }
+    }
+}
